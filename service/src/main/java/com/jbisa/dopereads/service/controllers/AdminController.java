e20d95085ac221b9e3c64a6cb08c7c98dcb5c0d5
Jay Bisa <jay.bisa@gmail.com>
2021-12-23 13:33:15 -0500

    Rename HealthCheckController to AdminController.

--- service/src/main/java/com/jbisa/dopereads/service/controllers/AdminController.java
+++ service/src/main/java/com/jbisa/dopereads/service/controllers/AdminController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthCheckController {
+public class AdminController {
 
-    @GetMapping("/health-check")
+    @GetMapping("/admin/health-check")
     public String healthCheck() {
         return "Service is healthy!";
     }
