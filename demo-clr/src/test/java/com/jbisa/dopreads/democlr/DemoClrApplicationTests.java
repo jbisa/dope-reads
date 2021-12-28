a96bb5288374cac1bd104a236a3dc86dcd5b340d
Jay Bisa <jay.bisa@gmail.com>
2021-12-27 21:10:07 -0500

    Added a demo command line runner app.

---
+++ demo-clr/src/test/java/com/jbisa/dopreads/democlr/DemoClrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbisa.dopreads.democlr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoClrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
